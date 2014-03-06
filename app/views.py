2afb691a455b8642ec25c56a31004f8bf56c6fd7
Wulong Qiu <owngoalcn@gmail.com>
2014-03-06 18:25:13 +0800

    列表数据输入，并解决模板中出现中文字符导致程序不能运行的问题。

--- app/views.py
+++ app/views.py
@@ -1,6 +1,26 @@
+# -*- coding: utf-8 -*-
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+# 以上三行用来解决模板中出现中文时提示unicodedecodeerror错误的问题
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
+    user = { 'nickname' : '乌龙球'}
+    posts = [
+        {
+            'author' : {'nickname' : '球球'},
+            'body' : '一个新的开始，一个好的开始！'
+        },
+        {
+            'author' : {'nickname' : '大球球'},
+            'body' : 'Python的Flask框架！'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)
